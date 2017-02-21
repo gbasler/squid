2171413a923411f6318c940498b7a368f0771ad2
LPTK <lionel.parreaux@gmail.com>
2017-02-21 14:36:56 +0100

    Moving `run` from Producer to BoundedProducer and making its iface safer

--- example/src/main/scala/sfusion/Producer.scala
+++ example/src/main/scala/sfusion/Producer.scala
@@ -19,39 +19,76 @@ abstract class Producer[+A](underFun: => impl.Producer[A]) { prodSelf =>
   type Filtered[+B] <: Producer[B]
   def withFilter(pred: A => Bool) = filter(pred)
   def filter(pred: A => Bool): Filtered[A]
+  def dropWhile(pred: A => Bool): Filtered[A]
+  def takeWhile(pred: A => Bool): Filtered[A]
   
   type Sliced[+B] <: Producer[B]
   def slice(from: Int, to: Int): Sliced[A]
   
-  type SoftSliced[+B] <: Producer[B]
-  def take(num: Int): SoftSliced[A] // TODO more precise type (bounded)
-  def drop(num: Int): SoftSliced[A]
-  def dropWhile(pred: A => Bool): Filtered[A]
-  def takeWhile(pred: A => Bool): Filtered[A]
+  type Taken[+B] <: BoundedProducer[B]
+  def take(num: Int): Taken[A]
+  
+  type Dropped[+B] <: Producer[B]
+  def drop(num: Int): Dropped[A]
+  
+  abstract class Consumer[+R] {
+    
+    def fold[S](z:S)(s:(S,R)=>S): Aggr[S]
+    def build[S,T](z:S)(s:(S,R)=>Unit)(mk: S => T): Aggr[T]
+    def map[S](f: R => S): Consumer[S]
+    def flatMap[S](f: (=> R) => Aggr[S]): Aggr[S]
+    def withFilter(pred: R => Bool) = filter(pred)
+    def filter(pred: R => Bool): Consumer[Option[R]]
+    
+    def count: Aggr[Int] = fold(0)((s, _) => s+1)
+    def sum[B >: R](implicit num: Numeric[B]): Aggr[B] = {
+      val B = implicitly[Numeric[B]]
+      fold(B.zero)(B.plus)
+    }
+    def toList: Aggr[List[R]] = build(ListBuffer.empty[R])(_ += _)(_.result)
+
+    def toBufferOf[S>:R]: Aggr[mutable.ArrayBuffer[S]] = build(mutable.ArrayBuffer.empty[S])(_ += _)(identity)
+    // ^ does not work well with type inference in for-comprehensions, so we also offer the following:
+    def toBuffer: Aggr[mutable.ArrayBuffer[_<:R]] = toBufferOf[R]
+    
+  }
+  trait Aggr[+R] extends Consumer[R] {
+    def map[S](f: R => S): Aggr[S]
+    def filter(pred: R => Bool): Aggr[Option[R]]
+  }
   
   /** Not public as it is unsafe to aggregate a potentially infinite Producer.
     * Use a size-limiting method such as `take` first, or make your finiteness assumption explicit with `assumeBounded`. */
-  protected def run[R](k: Consumer[A] => Aggr[R]) = {
-    val aggr = k(new Consumer[A]{
-      private[this] var cur = null.asInstanceOf[A]  // TODO better error
-      private[Producer] def iter: A => Unit = a => cur = a
-      private[Producer] def get: A = cur
-    })
-    impl.foreach(under)(aggr.iter)
-    aggr.get
-  }
+  protected def run[R](k: Consumer[A] => Aggr[R]): R
   
   def repeat[R](k: Consumer[A] => Aggr[R]): Producer[R] = ???
   
+  def asSized = this |>? { case self: SizedProducer[A] with this.type => self }
+  def size_? = asSized Then (_.size)
+  def asBounded = this |>? { case self: BoundedProducer[A] with this.type => self }
+  def sizeUpperBound_? = asBounded Then (_.sizeUpperBound)
+  
+  def assumeBounded: BoundedProducer[A] = new BoundedProducerImpl(under, Int.MaxValue)
+  
+}
+private abstract class AbstractProducer[A](under: => impl.Producer[A]) extends Producer[A](under) { prodSelf =>
+  
+  protected def run[R](k: super.Consumer[A] => super.Aggr[R]): R = {
+    val aggr = k(new Consumer[A]{
+      private[this] var cur = null.asInstanceOf[A]  // TODO better error
+      def iter: A => Unit = a => cur = a
+      def get: A = cur
+    })
+    val ag = aggr.asInstanceOf[Aggr[R]] // could avoid this one by using `type Consumer[+R] <: ConsumerAPI[R]` in Producer
+    impl.foreach(under)(ag.iter)
+    ag.get
+  }
+  
   /** TODO zip,take,drop,etc. */
-  trait Consumer[+R] {
-    // TODO move these details into invariant ProducerImpl
+  abstract class Consumer[+R] extends super.Consumer[R] {
+    def iter: A => Unit
+    def get: R
     
-    // Should not need `uncheckedVariance`, if Scala allowed:
-    //private[Producer.this] def iter: A => Unit
-    private[Producer] def iter: (A @uncheckedVariance) => Unit
-    private[Producer] def get: R
-
     def fold[S](z:S)(s:(S,R)=>S): Aggr[S] = {
       var cur = z
       new Aggr(a => {iter(a); cur = s(cur,get)},cur)
@@ -62,13 +99,11 @@ abstract class Producer[+A](underFun: => impl.Producer[A]) { prodSelf =>
     }
 
     def map[S](f: R => S): Aggr[S] = new Aggr(iter,f(get))
-    def flatMap[S](f: (=> R) => Aggr[S]): Aggr[S] = {
+    def flatMap[S](f: (=> R) => AbstractProducer.super.Aggr[S]): Aggr[S] = {
       var cell = null.asInstanceOf[R]
       val newAggr = f(cell).asInstanceOf[Aggr[S]]
       new Aggr(a => {iter(a); cell = get; newAggr.iter(a)},newAggr.get)
     }
-
-    def withFilter(pred: R => Bool) = filter(pred)
 
     def filter(pred: R => Bool): Aggr[Option[R]] = {
       var filtered = Option.empty[R]
@@ -78,46 +113,29 @@ abstract class Producer[+A](underFun: => impl.Producer[A]) { prodSelf =>
         filtered = if (pred(g)) Some(g) else None
       }, filtered)
     }
-
-    def count: Aggr[Int] = fold(0)((s, _) => s+1)
-    def sum[B >: R](implicit num: Numeric[B]): Aggr[B] = {
-      val B = implicitly[Numeric[B]]
-      fold(B.zero)(B.plus)
-    }
-    def toList: Aggr[List[R]] = build(ListBuffer.empty[R])(_ += _)(_.result)
-
-    def toBufferOf[S>:R]: Aggr[mutable.ArrayBuffer[S]] = build(mutable.ArrayBuffer.empty[S])(_ += _)(identity)
-    // ^ does not work well with type inference in for-comprehensions:
-    def toBuffer: Aggr[mutable.ArrayBuffer[_<:R]] = toBufferOf[R]
-
+    
   }
-  class Aggr[+R](iterFun: A => Unit, getFun: => R) extends Consumer[R] {
-    private[Producer] def iter: (A @uncheckedVariance) => Unit = iterFun
-    private[Producer] def get: R = getFun
+  class Aggr[+R](iterFun: A => Unit, getFun: => R) extends Consumer[R] with super.Aggr[R] {
+    def iter: A => Unit = iterFun
+    def get: R = getFun
   }
-  
-  def asSized = this |>? { case self: SizedProducer[A] with this.type => self }
-  def size_? = asSized Then (_.size)
-  def asBounded = this |>? { case self: BoundedProducer[A] with this.type => self }
-  def sizeUpperBound_? = asBounded Then (_.sizeUpperBound)
-  
-  def assumeBounded: BoundedProducer[A] = new BoundedProducerImpl(under, Int.MaxValue)
   
 }
-private class ProducerImpl[+A](under: => impl.Producer[A]) extends Producer[A](under) { prodSelf =>
+private class ProducerImpl[A](under: => impl.Producer[A]) extends AbstractProducer[A](under) { prodSelf =>
   import Producer._
   
   //type Mapped[B] >: Producer[B] with Sized with Indexed with Order[B]#Sorted with Equiv[B]#Distinct <: Producer[B]
   type Mapped[+B] = Producer[B]
   type Filtered[+B] = Producer[B]
   type Sliced[+B] = Producer[B]
-  type SoftSliced[+B] = Producer[B]
+  type Taken[+B] = BoundedProducer[B]
+  type Dropped[+B] = Producer[B]
   
   def map[B](f: A => B): Mapped[B] = new ProducerImpl(impl.map(under)(f))
   def filter(pred: A => Bool): Filtered[A] = new ProducerImpl(impl.filter(under)(pred))
   def slice(from: Int, to: Int): Sliced[A] = ???
-  def take(num: Int): SoftSliced[A] = new ProducerImpl(impl.take(under)(num))
-  def drop(num: Int): SoftSliced[A] = ??? //rebuild(impl.drop(under)(num), size_? Then (_ - num min 0), sizeUpperBound_? Then (_ - num min 0), index_? map (_ compose ((_:Int) - num)))
+  def take(num: Int): Taken[A] = new BoundedProducerImpl(impl.take(under)(num), num)
+  def drop(num: Int): Dropped[A] = new ProducerImpl(impl.drop(under)(num))
   def dropWhile(pred: A => Bool): Filtered[A] = new ProducerImpl(impl.dropWhile(under)(pred))
   def takeWhile(pred: A => Bool): Filtered[A] = new ProducerImpl(impl.takeWhile(under)(pred))
   
@@ -132,55 +150,64 @@ object Producer {
 }
 import Producer._
 
-abstract class BoundedProducer[+A](underFun: => impl.Producer[A], val sizeUpperBound: Int) extends Producer[A](underFun) {
+trait BoundedProducer[+A] extends Producer[A] {
+  val sizeUpperBound: Int
+  
   type Mapped[+B] <: BoundedProducer[B]
   type Filtered[+B] <: BoundedProducer[B]
   type Sliced[+B] <: SizedProducer[B]
-  type SoftSliced[+B] <: BoundedProducer[B]
+  type Taken[+B] <: BoundedProducer[B]
+  type Dropped[+B] <: BoundedProducer[B]
   
-  // Making `run` public
-  override def run[R](k: Consumer[A] => Aggr[R]) = super.run(k)
+  // Making `run` public because it is now safe:
+  abstract override def run[R](k: Consumer[A] => Aggr[R]) = super.run(k)
   
   def groupBy[K,R](by: A => K)(k: (K,Consumer[A]) => Aggr[R]): Map[K,R] = hashBy(by)(k)
   def hashBy[K,R](by: A => K)(k: (K,Consumer[A]) => Aggr[R]): Map[K,R] = ???
   def trieBy[K,R](by: A => K)(k: (K,Consumer[A]) => Aggr[R]): Map[K,R] = ???
   
 }
-private class BoundedProducerImpl[+A](underFun: => impl.Producer[A], sizeUpperBound: Int) extends BoundedProducer[A](underFun,sizeUpperBound) {
+private class BoundedProducerImpl[A](underFun: => impl.Producer[A], val sizeUpperBound: Int) extends AbstractProducer[A](underFun) with BoundedProducer[A] {
   
   type Mapped[+B] = BoundedProducer[B]
   type Filtered[+B] = BoundedProducer[B]
   type Sliced[+B] = SizedProducer[B]
-  type SoftSliced[+B] = BoundedProducer[B]
+  type Taken[+B] = BoundedProducer[B]
+  type Dropped[+B] = BoundedProducer[B]
   
   def map[B](f: A => B): Mapped[B] = new BoundedProducerImpl(impl.map(under)(f), sizeUpperBound)
-  def filter(pred: A => Bool): Filtered[A] = new BoundedProducerImpl(impl.filter(under)(pred),sizeUpperBound)
+  def filter(pred: A => Bool): Filtered[A] = new BoundedProducerImpl(impl.filter(under)(pred), sizeUpperBound)
   def slice(from: Int, to: Int): Sliced[A] = ???
-  def take(num: Int): SoftSliced[A] = new BoundedProducerImpl(impl.take(under)(num),num)
-  def drop(num: Int): SoftSliced[A] = new BoundedProducerImpl(impl.take(under)(num),sizeUpperBound - num max 0)
-  def dropWhile(pred: A => Bool): Filtered[A] = new BoundedProducerImpl(impl.dropWhile(under)(pred),sizeUpperBound)
-  def takeWhile(pred: A => Bool): Filtered[A] = new BoundedProducerImpl(impl.takeWhile(under)(pred),sizeUpperBound)
+  def take(num: Int): Taken[A] = new BoundedProducerImpl(impl.take(under)(num), num)
+  def drop(num: Int): Dropped[A] = new BoundedProducerImpl(impl.drop(under)(num), sizeUpperBound - num max 0)
+  def dropWhile(pred: A => Bool): Filtered[A] = new BoundedProducerImpl(impl.dropWhile(under)(pred), sizeUpperBound)
+  def takeWhile(pred: A => Bool): Filtered[A] = new BoundedProducerImpl(impl.takeWhile(under)(pred), sizeUpperBound)
   
 }
 
-abstract class SizedProducer[+A](underFun: => impl.Producer[A], val size: Int) extends BoundedProducer[A](underFun,size) {
+trait SizedProducer[+A] extends BoundedProducer[A] {
+  val size: Int
+  
   type Mapped[+B] <: SizedProducer[B]
   type Filtered[+B] <: BoundedProducer[B]
   type Sliced[+B] <: SizedProducer[B]
-  type SoftSliced[+B] <: SizedProducer[B]
+  type Taken[+B] <: SizedProducer[B]
+  type Dropped[+B] <: SizedProducer[B]
 }
-private class SizedProducerImpl[+A](underFun: => impl.Producer[A], size: Int) extends SizedProducer[A](underFun,size) {
+private class SizedProducerImpl[A](underFun: => impl.Producer[A], val size: Int) extends AbstractProducer[A](underFun) with SizedProducer[A] {
+  val sizeUpperBound = size
   
   type Mapped[+B] = SizedProducer[B]
   type Filtered[+B] = BoundedProducer[B]
   type Sliced[+B] = SizedProducer[B]
-  type SoftSliced[+B] = SizedProducer[B]
+  type Taken[+B] = SizedProducer[B]
+  type Dropped[+B] = SizedProducer[B]
   
   def map[B](f: A => B): SizedProducer[B] = new SizedProducerImpl(impl.map(under)(f), size)
   def filter(pred: A => Bool): Filtered[A] = new BoundedProducerImpl(impl.filter(under)(pred),sizeUpperBound)
   def slice(from: Int, to: Int): Sliced[A] = ???
-  def take(num: Int): SoftSliced[A] = new SizedProducerImpl(impl.take(under)(num), size min num)
-  def drop(num: Int): SoftSliced[A] = new SizedProducerImpl(impl.take(under)(num), size - num max 0)
+  def take(num: Int): Taken[A] = new SizedProducerImpl(impl.take(under)(num), size min num)
+  def drop(num: Int): Dropped[A] = new SizedProducerImpl(impl.drop(under)(num), size - num max 0)
   def dropWhile(pred: A => Bool): Filtered[A] = new BoundedProducerImpl(impl.dropWhile(under)(pred),size)
   def takeWhile(pred: A => Bool): Filtered[A] = new BoundedProducerImpl(impl.takeWhile(under)(pred),size)
   
@@ -188,7 +215,8 @@ private class SizedProducerImpl[+A](underFun: => impl.Producer[A], size: Int) ex
 
 object Test extends App {
   
-  val s = fromIndexed(1 to 3).map(x => x oh_and print(s"[$x]"))
+  //val s = fromIndexed(1 to 3).map(x => x oh_and print(s"[$x]")).take(4)
+  val s = fromIndexed(1 to 5).map(x => x oh_and print(s"[$x]")).take(4).drop(1)
   val p = s.map(_+1)
   
   //println(Producer.fromIndexed(1 to 3).map(_+1).size)
@@ -207,6 +235,10 @@ object Test extends App {
   //println(q2.size, q2.index)
   println(q2.size)
   
+  // Prevented statically:  TODO test
+  //println(q.run(_.map(_+1)))
+  //println(fromIndexed(1 until 1).run(_.map(_+1)))
+  //println(fromIndexed(1 until 1).map(_.toString).run(_.map(identity)))
   
   //println(p.run(_.count))
 
@@ -219,6 +251,7 @@ object Test extends App {
   ///*
   val r = p.run(p => for {
     a <- p
+    //() = println(p) // TODO B/E
     n <- p.count
     //() = println(n)
     s <- p.sum
