883211160b509b9eea2c23b319a8d40e81ba4774
fschueler <fschueler@posteo.de>
2017-10-16 18:29:18 +0200

    Add simple Pretty Printer
    (WIP) Add more translations
    
    (WIP) Change all Rep types to String
    
    (WIP) fix printing for let-bindings
    
    (WIP) Add tests for lambdas
    
    (WIP) Improve pretty printing for variables and statements
    
    Add type ascriptions
    
    (WIP) Add offset for indentation
    
    (WIP) Add offset formatting and line-breaks

---
+++ core/src/main/scala/squid/ir/PrettyPrinter.scala
@@ -0,0 +1,190 @@
+package squid
+package ir
+
+import utils._
+import squid.lang.Base
+import meta.RuntimeUniverseHelpers._
+import sru._
+
+class PrettyPrinter extends Base with TraceDebug {
+
+  override type Rep = Int => String
+  override type BoundVal = (String, TypeRep) // (name, type)
+  override type TypeRep  = TypSymbol
+  type TypSymbol = String
+  type MtdSymbol = String
+
+  def repEq(a: Rep, b: Rep): Boolean = a == b
+  def typLeq(a: TypeRep, b: TypeRep): Boolean = a == b
+
+  val indent = 2
+
+  private def removeBrackets(exp: String): String = exp.drop(1).dropRight(1)
+
+  val escape = (str: String) => str
+    .replace("\b", "\\b")
+    .replace("\n","\\n")
+    .replace("\t", "\\t")
+    .replace("\r","\\r")
+    .replace("\f", "\\f")
+    .replace("\"","\\\"")
+    .replace("\\","\\\\")
+
+  override def bindVal(name: String, typ: TypeRep, annots: List[Annot]): BoundVal = (name, typ)
+
+  override def readVal(v: BoundVal): Rep = offset => v._1 // get the name as string
+
+  override def const(value: Any): Rep = offset => value match {
+    case _: Char   => s"""'$value'"""
+    case v: String => s""""${escape(v)}""""
+    case _ => value.toString
+  }
+
+  override def lambda(params: List[BoundVal], body: => Rep): Rep = offset => {
+    val ps = params.map(p => s"${p._1}: ${p._2}") // map tp "name: Type"
+    val plist = ps.mkString(", ") // make list of all params
+    s"(($plist) => ${body(offset)})"
+  }
+
+  override def staticModule(fullName: String): Rep = offset => fullName
+
+  override def module(prefix: Rep, name: String, typ: TypeRep): Rep = offset => s"${prefix(offset)}.$name"
+
+  override def newObject(tp: TypeRep): Rep = offset => s"new $tp"
+
+  override def letin(bound: BoundVal, value: Rep, body: => Rep, bodyType: TypeRep): Rep = offset => {
+    val b = body(offset)
+    val v = value(offset)
+
+    if (bound._2.startsWith("squid.lib.Var")) {
+      val tpe = removeBrackets(bound._2.dropWhile(_ != '['))
+      " " * offset + s"var ${bound._1}: $tpe = $v\n$b"
+    } else {
+      " " * offset + s"val ${bound._1}: ${bound._2} = $v\n$b"
+    }
+  }
+
+  override def ascribe(self: Rep, typ: TypeRep): Rep = offset => s"(${self(offset)}: $typ)"
+
+  override def methodApp(self: Rep, mtd: MtdSymbol, targs: List[TypeRep], args: List[ArgList], tp: TypeRep): Rep = offset => {
+
+    val argumentLists = args.map(_.reps) // Seq[Int => String]
+    val arguments     = argumentLists.map(seq => seq.map(arg => arg(offset)))
+
+    val tArgList =  if (targs.nonEmpty) s"[${targs.mkString(", ")}]" else ""
+
+    val mtdDecoded = sru.TermName(mtd).decodedName
+
+    (self(offset), mtd) match {
+      case (squidLib, "IfThenElse") => {
+        val if_   = args.head.reps(0)(offset + indent)
+        val then_ = args.head.reps(1)(offset + indent)
+        val else_ = args.head.reps(2)(offset + indent)
+
+        var str = Seq(
+            s"if ($if_) {",
+            s"$then_",
+            " " * offset + "}"
+          )
+
+        if (else_ != "()") str = str.dropRight(1) ++
+          Seq(
+            " " * offset + "} else {",
+            s"$else_",
+            " " * offset + "}"
+          )
+
+        if (!else_.contains("\n") && !then_.contains("\n"))
+          str.mkString(" ")
+        else
+          str.mkString("\n")
+      }
+
+      case (squidLib, "While") => {
+        val cond_ = args.head.reps(0)(offset + indent)
+        val stmt_ = args.head.reps(1)(offset + indent)
+
+        val str = Seq(
+          s"while ($cond_) {",
+          s"$stmt_",
+          " " * offset + "}"
+        )
+
+        if (stmt_.contains("\n"))
+          str.mkString("\n")
+        else
+          str.mkString(" ")
+      }
+
+      case (squidLib, "Imperative") => {
+        val statements = args(0).reps.map(s => s(offset)).mkString("\n")
+        val result = args(1).reps.head(offset)
+
+        if (statements != "")
+          Seq(
+            " " * offset + statements,
+            " " * offset + result
+          ).mkString("\n")
+        else result
+      }
+
+      case ("squid.lib.Var", "apply") => args.head.reps.head(offset)
+
+      case ("scala.Symbol", "apply") =>
+        // scala.Symbol.apply("arg")
+        s"'${removeBrackets(args.head.reps.head(offset))}"
+
+      // apply on tuples should not print the TupleXX[A].apply
+      case (s, "apply") if s.startsWith("scala.Tuple") => s"${printArgLists(args, offset)}"
+
+      case (_, "<init>") | (_, "apply") =>
+        s"""${self(offset)}$tArgList${printArgLists(args, offset)}"""
+
+      // assignments: replace x.:=(y) with x = y
+      case (_, "$colon$eq") if tp == "scala.Unit" =>
+        s"""${self(offset)} = ${args.head.reps.head(offset)}"""
+
+      // replace x.! with x
+      case (_, "$bang") if targs.isEmpty && args.isEmpty => self(offset)
+
+      case _ => {
+        val argStr = printArgLists(args, offset)
+        s"""${self(offset)}.$mtdDecoded$tArgList$argStr"""
+      }
+    }
+  }
+
+  val printArgs = (params: ArgList, offset: Int) =>
+    (params.reps map (param => param(offset))).mkString("(", ", ", ")")
+
+  val printArgLists = (paramss: List[ArgList], offset: Int) =>
+    (paramss map (params => printArgs(params, offset))).mkString
+
+  override def byName(arg: => Rep): Rep = offset => arg(offset)
+
+  def uninterpretedType[A: sru.TypeTag]: TypeRep = sru.typeOf[A].typeSymbol.asType.fullName
+  def moduleType(fullName: String): TypeRep = fullName
+  def typeApp(self: TypeRep, typ: TypSymbol, targs: List[TypeRep]): TypeRep = typ
+
+  def staticTypeApp(typ: TypSymbol, targs: List[TypeRep]): TypeRep =
+    if (targs.nonEmpty) s"$typ[${targs.mkString(", ")}]" else typ
+
+  def recordType(fields: List[(String, TypeRep)]): TypeRep = ???
+  def constType(value: Any, underlying: TypeRep): TypeRep = ???
+
+  object Const extends ConstAPI {
+    def unapply[T: sru.TypeTag](ir: IR[T, _]): Option[T] = ???
+  }
+
+  def hole(name: String, typ: TypeRep): Rep = offset => s"?$name"
+  def hopHole(name: String, typ: TypeRep, yes: List[List[BoundVal]], no: List[BoundVal]): Rep = ???
+  def splicedHole(name: String, typ: TypeRep): Rep = ???
+  def substitute(r: => Rep,defs: scala.collection.immutable.Map[String,Rep]): Rep = ???
+  def typeHole(name: String): TypeRep = ???
+
+  /** Parameter `static` should be true only for truly static methods (in the Java sense)
+    * Note: index should be None when the symbol is not overloaded, to allow for more efficient caching */
+  override def loadMtdSymbol(typ: String, symName: String, index: Option[Int], static: Bool) = symName
+
+  override def loadTypSymbol(fullName: String) = fullName
+}
---
+++ src/test/scala/squid/ir/PrettyPrinterTests.scala
@@ -0,0 +1,392 @@
+package squid
+package ir
+
+import org.scalatest.FunSuite
+import MacroTesters._
+
+class PrettyPrinterTests extends FunSuite {
+  object b extends ir.SimpleAST
+  
+  val Inter = new ir.PrettyPrinter
+  
+  def same[A](xy: (A, A)) = assert(xy._1 == xy._2)
+  def runSame[A](xy: (b.Rep, A), exp: String) = {
+    same((b.reinterpret(xy._1, Inter)())(0), exp)
+  }
+  
+  test("Constants") ({
+    
+    runSame( shallowAndDeep(b){ 42 }, "42" )
+    
+    runSame( shallowAndDeep(b){ "ok" }, """"ok"""" )
+    
+    runSame( shallowAndDeep(b){ 'c' }, "'c'" )
+    
+    runSame( shallowAndDeep(b){ 'Cool }, "'Cool" )
+    
+  })
+  
+  test("Basic") {/*Inter.debugFor*/{
+
+    val act0 = shallowAndDeep(b){ "ok".reverse }
+    val exp0 =
+      """
+        |scala.Predef.augmentString("ok").reverse
+      """.stripMargin.trim
+
+    val act1 = shallowAndDeep(b){ "ok".take(1)+"ko" }
+    val exp1 =
+      """
+        |scala.Predef.augmentString("ok").take(1).+("ko")
+      """.stripMargin.trim
+
+    val act2 = shallowAndDeep(b){ {0 -> 1} swap }
+    val exp2 =
+      """
+        |scala.Predef.ArrowAssoc[scala.Int](0).->[scala.Int](1).swap
+      """.stripMargin.trim
+
+    val act3 = shallowAndDeep(b){ Array.ofDim[Float](10) }
+    val exp3 =
+      """
+        |scala.Array.ofDim[scala.Float](10)(scala.reflect.ClassTag.Float)
+      """.stripMargin.trim
+
+    val act4 = shallowAndDeep(b){ new scala.Array[Int](10)}
+    val exp4 =
+      """
+        |new scala.Array[scala.Int](10)
+      """.stripMargin.trim
+
+    runSame(act0, exp0)
+    runSame(act1, exp1)
+    runSame(act2, exp2)
+    runSame(act3, exp3)
+    runSame(act4, exp4)
+  }}
+
+  test("Bindings") {
+
+    //runSame( shallowAndDeep(b){ (arg: {val y:Int}) => arg.y } )  // Unsupported feature: Refinement type 'AnyRef{val y: Int}'
+
+    val act0 = shallowAndDeep(b){ val x = 0; x + 1 }
+    val exp0 =
+      """
+        |val x: scala.Int = 0
+        |x.+(1)
+      """.stripMargin.trim
+
+    val act1 = shallowAndDeep(b){ ((x: Int) => x + 1)(42) }
+    val exp1 =
+      """
+        |val x: scala.Int = 42
+        |x.+(1)
+      """.stripMargin.trim
+
+    val act2 = shallowAndDeep(b){ {x: Int => x + 1}.apply(42) }
+    val exp2 =
+      """
+        |val x: scala.Int = 42
+        |x.+(1)
+      """.stripMargin.trim
+
+    val act3 = shallowAndDeep(b){ val f = (x: Int) => x + 1}
+    val exp3 =
+      """
+        |val f: scala.Function1[scala.Int, scala.Int] = ((x: scala.Int) => x.+(1))
+        |()
+      """.stripMargin.trim
+
+    val act4 = shallowAndDeep(b){ val f = (x: Int) => x + 1; f(42)}
+    val exp4 =
+      """
+        |val f: scala.Function1[scala.Int, scala.Int] = ((x: scala.Int) => x.+(1))
+        |f(42)
+      """.stripMargin.trim
+
+    runSame(act0, exp0)
+    runSame(act1, exp1)
+    runSame(act2, exp2)
+    runSame(act3, exp3)
+    runSame(act4, exp4)
+  }
+
+  test("Variables") {
+
+    val act0 = shallowAndDeep(b){ lib.Var(0) }
+    val exp0 =
+      """
+        |0
+      """.stripMargin.trim
+
+    val act1 = shallowAndDeep(b){ var x = ("ok" + "ko".reverse).length; x-=1; (x+=1, x, 'lol) }
+    val exp1 =
+      """
+        |var x: scala.Int = "ok".+(scala.Predef.augmentString("ko").reverse).length()
+        |x = x.-(1)
+        |(x = x.+(1), x, 'lol)
+      """.stripMargin.trim
+
+    runSame(act0, exp0)
+    runSame(act1, exp1)
+  }
+
+  test("By-name") {{
+
+    val act0 = shallowAndDeep(b){ Dummies.byNameMethod(42) }
+    val exp0 =
+      """
+        |squid.Dummies.byNameMethod(42)
+      """.stripMargin.trim
+
+    val act1 = shallowAndDeep(b){ Dummies.byNameMethod(666) }
+    val exp1 =
+      """
+        |squid.Dummies.byNameMethod(666)
+      """.stripMargin.trim
+
+    runSame(act0, exp0)
+    runSame(act1, exp1)
+  }}
+
+  test("Varargs") {
+
+    val act0 = shallowAndDeep(b){ lib.Imperative()(42) }
+    val exp0 =
+      """
+        |42
+      """.stripMargin.trim
+
+    val act1 = shallowAndDeep(b){ var x = 0; lib.Imperative(x += 1)(x) }
+    val exp1 =
+      """
+        |var x: scala.Int = 0
+        |x = x.+(1)
+        |x
+      """.stripMargin.trim
+
+    val act2 = shallowAndDeep(b){ var x = 0; lib.Imperative(x += 1, x += 1)(x) }
+    val exp2 =
+      """
+        |var x: scala.Int = 0
+        |x = x.+(1)
+        |x = x.+(1)
+        |x
+      """.stripMargin.trim
+
+    val act3 = shallowAndDeep(b){ var x = 0; val modifs = Seq(x += 1, x += 1); lib.Imperative(modifs: _*)(x) }
+    val exp3 =
+      """
+        |var x: scala.Int = 0
+        |val modifs: scala.collection.Seq[scala.Unit] = scala.collection.Seq[scala.Unit](x = x.+(1), x = x.+(1))
+        |modifs
+        |x
+      """.stripMargin.trim
+
+    runSame(act0, exp0)
+    runSame(act1, exp1)
+    runSame(act2, exp2)
+    runSame(act3, exp3)
+  }
+
+  test("Virtualized Constructs") {
+
+    // Ascription
+    val act0 = shallowAndDeep(b){ (List(1,2,3) : Seq[Any]).size: Int }
+    val exp0 =
+      """
+        |(scala.collection.immutable.List[scala.Int](1, 2, 3): scala.collection.Seq[scala.Any]).size
+      """.stripMargin.trim
+
+    val act1 = shallowAndDeep(b){ "ok".length: Unit }
+    val exp1 =
+      """
+        |"ok".length()
+        |()
+      """.stripMargin.trim
+
+    // If then else
+    val act2 = shallowAndDeep(b){ if (Math.PI > 0) "ok" else "ko" }
+    val exp2 =
+      """
+        |if (true) { "ok" } else { "ko" }
+      """.stripMargin.trim
+
+    val act3 = shallowAndDeep(b){ var x = 0; if (true) x += 1 else x += 1; x }
+    val exp3 =
+      """
+        |var x: scala.Int = 0
+        |if (true) { x = x.+(1) } else { x = x.+(1) }
+        |x
+      """.stripMargin.trim
+
+    // While
+    val act4 = shallowAndDeep(b){ var x = 0; while (x < 3) { x += 1; println(x) }; x }
+    val exp4 =
+      """
+        |var x: scala.Int = 0
+        |while (x.<(3)) {
+        |  x = x.+(1)
+        |  scala.Predef.println(x)
+        |}
+        |x
+      """.stripMargin.trim
+
+    val act5 = shallowAndDeep(b){ var x = if (true) 5 else 6; x }
+    val exp5 =
+      """
+        |var x: scala.Int = if (true) { 5 } else { 6 }
+        |x
+      """.stripMargin.trim
+
+    val act6 = shallowAndDeep(b){ var x = 5; if (5 * x > 0) x += 1; x}
+    val exp6 =
+      s"""
+         |var x: scala.Int = 5
+         |if (5.*(x).>(0)) { x = x.+(1) }
+         |x
+       """.stripMargin.trim
+
+    val act7 = shallowAndDeep(b){ var x = Math.random(); if (x > 0) { println("greater!"); x = 0 } else { println("smaller!"); x = 1 }; x }
+    val exp7 =
+      """
+        |var x: scala.Double = java.lang.Math.random()
+        |if (x.>(0)) {
+        |  scala.Predef.println("greater!")
+        |  x = 0.0
+        |} else {
+        |  scala.Predef.println("smaller!")
+        |  x = 1.0
+        |}
+        |x
+      """.stripMargin.trim
+
+    val act8 = shallowAndDeep(b) { var i = 0; var y = 100; while (i < 3) { if (y % 10 == 0) { y /= 10; y * y }; i += 1}; y}
+    val exp8 =
+      """
+        |var i: scala.Int = 0
+        |var y: scala.Int = 100
+        |while (i.<(3)) {
+        |  if (y.%(10).==(0)) {
+        |    y = y./(10)
+        |    y.*(y)
+        |  }
+        |  i = i.+(1)
+        |}
+        |y
+      """.stripMargin.trim
+
+    val act9 = shallowAndDeep(b) {
+      val A = Array(Array(1, 2, 3), Array(1, 2, 3), Array(1, 2, 3))
+      var i = 0; var j = 0
+      while (i < 3) {
+        while (j < 3) {
+          A(i)(j) = i * j
+          j += 1
+        }
+        i += 1
+      }
+      A
+    }
+    val exp9 =
+      """
+        |val A: scala.Array[scala.Array[scala.Int]] = scala.Array[scala.Array[scala.Int]](scala.Array(1, 2, 3), scala.Array(1, 2, 3), scala.Array(1, 2, 3))(scala.reflect.ClassTag[scala.Array[scala.Int]](scala.runtime.ScalaRunTime.arrayClass(int)))
+        |var i: scala.Int = 0
+        |var j: scala.Int = 0
+        |while (i.<(3)) {
+        |  while (j.<(3)) {
+        |    A(i).update(j, i.*(j))
+        |    j = j.+(1)
+        |  }
+        |  i = i.+(1)
+        |}
+        |A
+      """.stripMargin.trim
+
+    runSame(act0, exp0)
+    runSame(act1, exp1)
+    runSame(act2, exp2)
+    runSame(act3, exp3)
+    runSame(act4, exp4)
+    runSame(act5, exp5)
+    runSame(act6, exp6)
+    runSame(act7, exp7)
+    runSame(act8, exp8)
+    runSame(act9, exp9)
+  }
+
+  test("Java") {{
+
+    // overloading
+    val act0 = shallowAndDeep(b){ "ok".indexOf('k'.toInt) }
+    val exp0 =
+      """
+        |"ok".indexOf('k'.toInt)
+      """.stripMargin.trim
+
+    val act1 = shallowAndDeep(b){ "ok".indexOf('k') }
+    val exp1 =
+      """
+        |"ok".indexOf(107)
+      """.stripMargin.trim
+
+    val act2 = shallowAndDeep(b){ "okok".indexOf("ok") }
+    val exp2 =
+      """
+        |"okok".indexOf("ok")
+      """.stripMargin.trim
+
+    val act3 = shallowAndDeep(b){ "okok".lastIndexOf("ok") }
+    val exp3 =
+      """
+        |"okok".lastIndexOf("ok")
+      """.stripMargin.trim
+
+    val act4 = shallowAndDeep(b){ String.valueOf(true) }
+    val exp4 =
+      """
+        |java.lang.String.valueOf(true)
+      """.stripMargin.trim
+
+    val act5 = shallowAndDeep(b){ "ok"+String.valueOf("ko") }
+    val exp5 =
+      """
+        |"ok".+(java.lang.String.valueOf("ko"))
+      """.stripMargin.trim
+
+    val act6 = shallowAndDeep(b){ ("ko" * 2) }
+    val exp6 =
+      """
+        |scala.Predef.augmentString("ko").*(2)
+      """.stripMargin.trim
+
+    val act7 = shallowAndDeep(b){ ("ok" + "ko"*2).length }
+    val exp7 =
+      """
+        |"ok".+(scala.Predef.augmentString("ko").*(2)).length()
+      """.stripMargin.trim
+
+    runSame(act0, exp0)
+    runSame(act1, exp1)
+    runSame(act2, exp2)
+    runSame(act3, exp3)
+    runSame(act4, exp4)
+    runSame(act5, exp5)
+    runSame(act6, exp6)
+    runSame(act7, exp7)
+  }}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
