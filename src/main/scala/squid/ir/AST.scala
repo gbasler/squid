0d6a40d77b580a1510f2ac1632c2063397a6e4d2
LPTK <lionel.parreaux@gmail.com>
2018-10-16 16:59:28 +0200

    Fix MethodApp#reps

--- src/main/scala/squid/ir/AST.scala
+++ src/main/scala/squid/ir/AST.scala
@@ -460,7 +460,7 @@ trait AST extends InspectableBase with ScalaTyping with ASTReinterpreter with Ru
   
   case class MethodApp(self: Rep, sym: MtdSymbol, targs: List[TypeRep], argss: List[ArgList], typ: TypeRep) extends NonTrivialDef with BasicDef {
     
-    def reps: Seq[Rep] = argss.flatMap(_.reps)
+    def reps: Seq[Rep] = self +: argss.flatMap(_.reps)
     def rebuild(reps: Seq[Rep]): BasicDef = {
       def rec(argss: List[ArgList], reps: Seq[Rep]): List[ArgList] = argss match {
         case Args(as@_*)::ass =>
@@ -477,7 +477,7 @@ trait AST extends InspectableBase with ScalaTyping with ASTReinterpreter with Ru
           val Seq() = reps
           Nil
       }
-      MethodApp(self, sym, targs, rec(argss, reps), typ)
+      MethodApp(reps.head, sym, targs, rec(argss, reps.tail), typ)
     }
     
     lazy val phase = { // TODO cache annotations for each sym
