c3152ed8d4187f1f08122a6ea57320dde32da6cc
Lionel Parreaux <lionel.parreaux@gmail.com>
2019-07-08 13:36:42 +0200

    WIP mono type more impressive, but code sizes not as compelling!

--- haskellopt/src/test/haskell/VectorsBench.hs
+++ haskellopt/src/test/haskell/VectorsBench.hs
@@ -1,103 +1,68 @@
 -- Generated Haskell code from SynthBench
 
-module Main(main) where
+module Main where
 
 import Criterion.Main
 
-prod_1 :: Num a => [a] -> a
-prod_1 [x0] = 0 + x0
-prod_1 [] = 0
-test_1 n = sum (map (\i -> prod_1 [i + 0]) [0..n])
+prod_1 :: (Int) -> Int
+prod_1 (x0) = 1 * x0
+test_1 n = sum (map (\i -> prod_1 (i ^ 0)) [0..n])
 
-prod_2 :: Num a => [a] -> a
-prod_2 [x0,x1] = 0 + x0 + x1
-prod_2 [x0] = 0 + x0
-prod_2 [] = 0
-test_2 n = sum (map (\i -> prod_2 [i + 0, i + 1]) [0..n])
+prod_2 :: (Int,Int) -> Int
+prod_2 (x0,x1) = 1 * x0 * x1
+test_2 n = sum (map (\i -> prod_2 (i ^ 0, i ^ 1)) [0..n])
 
-prod_3 :: Num a => [a] -> a
-prod_3 [x0,x1,x2] = 0 + x0 + x1 + x2
-prod_3 [x0,x1] = 0 + x0 + x1
-prod_3 [x0] = 0 + x0
-prod_3 [] = 0
-test_3 n = sum (map (\i -> prod_3 [i + 0, i + 1, i + 2]) [0..n])
+prod_3 :: (Int,Int,Int) -> Int
+prod_3 (x0,x1,x2) = 1 * x0 * x1 * x2
+test_3 n = sum (map (\i -> prod_3 (i ^ 0, i ^ 1, i ^ 2)) [0..n])
 
-prod_4 :: Num a => [a] -> a
-prod_4 [x0,x1,x2,x3] = 0 + x0 + x1 + x2 + x3
-prod_4 [x0,x1,x2] = 0 + x0 + x1 + x2
-prod_4 [x0,x1] = 0 + x0 + x1
-prod_4 [x0] = 0 + x0
-prod_4 [] = 0
-test_4 n = sum (map (\i -> prod_4 [i + 0, i + 1, i + 2, i + 3]) [0..n])
+prod_4 :: (Int,Int,Int,Int) -> Int
+prod_4 (x0,x1,x2,x3) = 1 * x0 * x1 * x2 * x3
+test_4 n = sum (map (\i -> prod_4 (i ^ 0, i ^ 1, i ^ 2, i ^ 3)) [0..n])
 
-prod_5 :: Num a => [a] -> a
-prod_5 [x0,x1,x2,x3,x4] = 0 + x0 + x1 + x2 + x3 + x4
-prod_5 [x0,x1,x2,x3] = 0 + x0 + x1 + x2 + x3
-prod_5 [x0,x1,x2] = 0 + x0 + x1 + x2
-prod_5 [x0,x1] = 0 + x0 + x1
-prod_5 [x0] = 0 + x0
-prod_5 [] = 0
-test_5 n = sum (map (\i -> prod_5 [i + 0, i + 1, i + 2, i + 3, i + 4]) [0..n])
+prod_5 :: (Int,Int,Int,Int,Int) -> Int
+prod_5 (x0,x1,x2,x3,x4) = 1 * x0 * x1 * x2 * x3 * x4
+test_5 n = sum (map (\i -> prod_5 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4)) [0..n])
 
-prod_6 :: Num a => [a] -> a
-prod_6 [x0,x1,x2,x3,x4,x5] = 0 + x0 + x1 + x2 + x3 + x4 + x5
-prod_6 [x0,x1,x2,x3,x4] = 0 + x0 + x1 + x2 + x3 + x4
-prod_6 [x0,x1,x2,x3] = 0 + x0 + x1 + x2 + x3
-prod_6 [x0,x1,x2] = 0 + x0 + x1 + x2
-prod_6 [x0,x1] = 0 + x0 + x1
-prod_6 [x0] = 0 + x0
-prod_6 [] = 0
-test_6 n = sum (map (\i -> prod_6 [i + 0, i + 1, i + 2, i + 3, i + 4, i + 5]) [0..n])
+prod_6 :: (Int,Int,Int,Int,Int,Int) -> Int
+prod_6 (x0,x1,x2,x3,x4,x5) = 1 * x0 * x1 * x2 * x3 * x4 * x5
+test_6 n = sum (map (\i -> prod_6 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4, i ^ 5)) [0..n])
 
-prod_7 :: Num a => [a] -> a
-prod_7 [x0,x1,x2,x3,x4,x5,x6] = 0 + x0 + x1 + x2 + x3 + x4 + x5 + x6
-prod_7 [x0,x1,x2,x3,x4,x5] = 0 + x0 + x1 + x2 + x3 + x4 + x5
-prod_7 [x0,x1,x2,x3,x4] = 0 + x0 + x1 + x2 + x3 + x4
-prod_7 [x0,x1,x2,x3] = 0 + x0 + x1 + x2 + x3
-prod_7 [x0,x1,x2] = 0 + x0 + x1 + x2
-prod_7 [x0,x1] = 0 + x0 + x1
-prod_7 [x0] = 0 + x0
-prod_7 [] = 0
-test_7 n = sum (map (\i -> prod_7 [i + 0, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6]) [0..n])
+prod_7 :: (Int,Int,Int,Int,Int,Int,Int) -> Int
+prod_7 (x0,x1,x2,x3,x4,x5,x6) = 1 * x0 * x1 * x2 * x3 * x4 * x5 * x6
+test_7 n = sum (map (\i -> prod_7 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4, i ^ 5, i ^ 6)) [0..n])
 
-prod_8 :: Num a => [a] -> a
-prod_8 [x0,x1,x2,x3,x4,x5,x6,x7] = 0 + x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7
-prod_8 [x0,x1,x2,x3,x4,x5,x6] = 0 + x0 + x1 + x2 + x3 + x4 + x5 + x6
-prod_8 [x0,x1,x2,x3,x4,x5] = 0 + x0 + x1 + x2 + x3 + x4 + x5
-prod_8 [x0,x1,x2,x3,x4] = 0 + x0 + x1 + x2 + x3 + x4
-prod_8 [x0,x1,x2,x3] = 0 + x0 + x1 + x2 + x3
-prod_8 [x0,x1,x2] = 0 + x0 + x1 + x2
-prod_8 [x0,x1] = 0 + x0 + x1
-prod_8 [x0] = 0 + x0
-prod_8 [] = 0
-test_8 n = sum (map (\i -> prod_8 [i + 0, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7]) [0..n])
+prod_8 :: (Int,Int,Int,Int,Int,Int,Int,Int) -> Int
+prod_8 (x0,x1,x2,x3,x4,x5,x6,x7) = 1 * x0 * x1 * x2 * x3 * x4 * x5 * x6 * x7
+test_8 n = sum (map (\i -> prod_8 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4, i ^ 5, i ^ 6, i ^ 7)) [0..n])
 
-prod_9 :: Num a => [a] -> a
-prod_9 [x0,x1,x2,x3,x4,x5,x6,x7,x8] = 0 + x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8
-prod_9 [x0,x1,x2,x3,x4,x5,x6,x7] = 0 + x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7
-prod_9 [x0,x1,x2,x3,x4,x5,x6] = 0 + x0 + x1 + x2 + x3 + x4 + x5 + x6
-prod_9 [x0,x1,x2,x3,x4,x5] = 0 + x0 + x1 + x2 + x3 + x4 + x5
-prod_9 [x0,x1,x2,x3,x4] = 0 + x0 + x1 + x2 + x3 + x4
-prod_9 [x0,x1,x2,x3] = 0 + x0 + x1 + x2 + x3
-prod_9 [x0,x1,x2] = 0 + x0 + x1 + x2
-prod_9 [x0,x1] = 0 + x0 + x1
-prod_9 [x0] = 0 + x0
-prod_9 [] = 0
-test_9 n = sum (map (\i -> prod_9 [i + 0, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8]) [0..n])
+prod_9 :: (Int,Int,Int,Int,Int,Int,Int,Int,Int) -> Int
+prod_9 (x0,x1,x2,x3,x4,x5,x6,x7,x8) = 1 * x0 * x1 * x2 * x3 * x4 * x5 * x6 * x7 * x8
+test_9 n = sum (map (\i -> prod_9 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4, i ^ 5, i ^ 6, i ^ 7, i ^ 8)) [0..n])
 
-prod_10 :: Num a => [a] -> a
-prod_10 [x0,x1,x2,x3,x4,x5,x6,x7,x8,x9] = 0 + x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
-prod_10 [x0,x1,x2,x3,x4,x5,x6,x7,x8] = 0 + x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8
-prod_10 [x0,x1,x2,x3,x4,x5,x6,x7] = 0 + x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7
-prod_10 [x0,x1,x2,x3,x4,x5,x6] = 0 + x0 + x1 + x2 + x3 + x4 + x5 + x6
-prod_10 [x0,x1,x2,x3,x4,x5] = 0 + x0 + x1 + x2 + x3 + x4 + x5
-prod_10 [x0,x1,x2,x3,x4] = 0 + x0 + x1 + x2 + x3 + x4
-prod_10 [x0,x1,x2,x3] = 0 + x0 + x1 + x2 + x3
-prod_10 [x0,x1,x2] = 0 + x0 + x1 + x2
-prod_10 [x0,x1] = 0 + x0 + x1
-prod_10 [x0] = 0 + x0
-prod_10 [] = 0
-test_10 n = sum (map (\i -> prod_10 [i + 0, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]) [0..n])
+prod_10 :: (Int,Int,Int,Int,Int,Int,Int,Int,Int,Int) -> Int
+prod_10 (x0,x1,x2,x3,x4,x5,x6,x7,x8,x9) = 1 * x0 * x1 * x2 * x3 * x4 * x5 * x6 * x7 * x8 * x9
+test_10 n = sum (map (\i -> prod_10 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4, i ^ 5, i ^ 6, i ^ 7, i ^ 8, i ^ 9)) [0..n])
+
+prod_11 :: (Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int) -> Int
+prod_11 (x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10) = 1 * x0 * x1 * x2 * x3 * x4 * x5 * x6 * x7 * x8 * x9 * x10
+test_11 n = sum (map (\i -> prod_11 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4, i ^ 5, i ^ 6, i ^ 7, i ^ 8, i ^ 9, i ^ 10)) [0..n])
+
+prod_12 :: (Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int) -> Int
+prod_12 (x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11) = 1 * x0 * x1 * x2 * x3 * x4 * x5 * x6 * x7 * x8 * x9 * x10 * x11
+test_12 n = sum (map (\i -> prod_12 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4, i ^ 5, i ^ 6, i ^ 7, i ^ 8, i ^ 9, i ^ 10, i ^ 11)) [0..n])
+
+prod_13 :: (Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int) -> Int
+prod_13 (x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12) = 1 * x0 * x1 * x2 * x3 * x4 * x5 * x6 * x7 * x8 * x9 * x10 * x11 * x12
+test_13 n = sum (map (\i -> prod_13 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4, i ^ 5, i ^ 6, i ^ 7, i ^ 8, i ^ 9, i ^ 10, i ^ 11, i ^ 12)) [0..n])
+
+prod_14 :: (Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int) -> Int
+prod_14 (x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13) = 1 * x0 * x1 * x2 * x3 * x4 * x5 * x6 * x7 * x8 * x9 * x10 * x11 * x12 * x13
+test_14 n = sum (map (\i -> prod_14 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4, i ^ 5, i ^ 6, i ^ 7, i ^ 8, i ^ 9, i ^ 10, i ^ 11, i ^ 12, i ^ 13)) [0..n])
+
+prod_15 :: (Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int,Int) -> Int
+prod_15 (x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14) = 1 * x0 * x1 * x2 * x3 * x4 * x5 * x6 * x7 * x8 * x9 * x10 * x11 * x12 * x13 * x14
+test_15 n = sum (map (\i -> prod_15 (i ^ 0, i ^ 1, i ^ 2, i ^ 3, i ^ 4, i ^ 5, i ^ 6, i ^ 7, i ^ 8, i ^ 9, i ^ 10, i ^ 11, i ^ 12, i ^ 13, i ^ 14)) [0..n])
 
 main = defaultMain [
     bench "1" $ whnf test_1 1000,
@@ -109,6 +74,11 @@ main = defaultMain [
     bench "7" $ whnf test_7 1000,
     bench "8" $ whnf test_8 1000,
     bench "9" $ whnf test_9 1000,
-    bench "10" $ whnf test_10 1000
+    bench "10" $ whnf test_10 1000,
+    bench "11" $ whnf test_11 1000,
+    bench "12" $ whnf test_12 1000,
+    bench "13" $ whnf test_13 1000,
+    bench "14" $ whnf test_14 1000,
+    bench "15" $ whnf test_15 1000
   ]
 
--- haskellopt/src/test/scala/squid/haskellopt/SynthBench.scala
+++ haskellopt/src/test/scala/squid/haskellopt/SynthBench.scala
@@ -9,15 +9,23 @@ object SynthBench {
   //  - add all smaller cases (it seems to make GHC inline much more aggressively...)
   
   //val sizes = 1 to 25
-  //val sizes = 1 to 15
+  val sizes = 1 to 15
   //val sizes = 20 to 40 by 5
   //val sizes = 1 to 30 by 5
   //val sizes = (1 to 10) ++ (12 to 20 by 2)
-  val sizes = (1 to 10)
+  //val sizes = (1 to 10)
+  //val sizes = (5 to 30 by 5)
+  //val sizes = (2 to 20 by 2)
   
-  
-  def genPgrm(sizes: Iterable[Int], fileName: String, bench: Bool, exportOnlyMain: Bool, forceInline: Bool) = {
+  def genPgrm(sizes: Iterable[Int], fileName: String, bench: Bool, exportOnlyMain: Bool, forceInline: Bool,
+              generic: Bool = false,
+              lists: Bool = false, multiCase: Bool = false,
+  ) = {
     val imports = "Criterion.Main" :: Nil
+    
+    //val (`(`,`)`) = if (lists) ("[","]") else ("(",")")
+    val `[` = if (lists) "[" else "("
+    val `]` = if (lists) "]" else ")"
     
     val defs = for {
       size <- sizes
@@ -29,18 +37,31 @@ object SynthBench {
       //usage = s"test_$size n = sum (map (\\i -> prod_$size [${
       //  indices map (i => s"i + $i") mkString ", "}] [${
       //  indices map (i => s"i + $i") mkString ", "}]) [0..n])"
+      
+      //(unit,op) = ("0,"+")
+      (unit,op) = ("1","*")
+      //(unit,op) = ("666","`mod`")
+      
       prod =
         (if (forceInline) s"{-# INLINE prod_$size #-}\n" else "") + 
-        s"prod_$size :: Num a => [a] -> a\n" +
-        //s"prod_$size [${xs mkString ","}] = ${
-        //  (xs zip ys).foldLeft("0"){case (acc,xy) => acc + " + " + xy._1}}"
-        (xs.reverse.tails.map { xsr =>
+        (if (lists) if (generic) s"prod_$size :: Num a => ${`[`}a${`]`} -> a\n" else s"prod_$size :: ${`[`}Int${`]`} -> Int\n"
+        else {
+          assert((`[`,`]`) === ("(",")"))
+          def tupled(str: String) = List.fill(size)(str).mkString("(",",",")")
+          if (generic) s"prod_$size :: Num a => ${tupled("a")} -> a\n" else s"prod_$size :: ${tupled("Int")} -> Int\n"
+        }) +
+        (if (!lists || !multiCase)
+        s"prod_$size ${`[`}${xs mkString ","}${`]`} = ${
+          (xs zip ys).foldLeft(unit){case (acc,xy) => acc + s" $op " + xy._1}}"
+        else (xs.reverse.tails.map { xsr =>
           val xs = xsr.reverse
-          s"prod_$size [${xs mkString ","}] = ${
-            (xs zip ys).foldLeft("0"){case (acc,xy) => acc + " + " + xy._1}}"
-        }).mkString("\n")
-      usage = s"test_$size n = sum (map (\\i -> prod_$size [${
-        indices map (i => s"i + $i") mkString ", "}]) [0..n])"
+          s"prod_$size ${`[`}${xs mkString ","}${`]`} = ${
+            (xs zip ys).foldLeft(unit){case (acc,xy) => acc + s" $op " + xy._1}}"
+        }.mkString("\n")))
+      usage = s"test_$size n = sum (map (\\i -> prod_$size ${`[`}${
+        //indices map (i => s"i + $i") mkString ", "
+        indices map (i => s"i ^ $i") mkString ", "
+      }${`]`}) [0..n])"
     } yield s"$prod\n$usage"
     
     val main = if (bench) {
@@ -81,6 +102,10 @@ object MkAllSynthBench extends App {
 
 object MeasureSynthBenchSizes extends BenchTests with App {
   
+  //val opt = "-O0"
+  //val opt = "-O1"
+  val opt = "-O2"
+  
   def getTerms(dumpLines: Iterator[String]): Int = {
     val sizes = dumpLines.dropWhile(_ =/= "Result size of CorePrep").drop(1).next
     println(s"SIZES: $sizes")
@@ -92,6 +117,7 @@ object MeasureSynthBenchSizes extends BenchTests with App {
   
   val coreSizes = for {
     size <- SynthBench.sizes
+    //size <- 5::Nil
   } yield {
     
     val fileName = s"VectorsBench_$size.hs"
@@ -100,7 +126,7 @@ object MeasureSynthBenchSizes extends BenchTests with App {
     def termsFor(forceInline: Bool) = {
       //SynthBench.genPgrm(size :: Nil, fileName, bench = false, exportOnlyMain = exportOnlyMain)
       SynthBench.genPgrm(size :: Nil, fileName, bench = true, exportOnlyMain = false, forceInline = forceInline)
-      %("ghc", "-O2", "-fforce-recomp", "-ddump-to-file", "-ddump-prep", path)(pwd)
+      %("ghc", opt, "-fforce-recomp", "-ddump-to-file", "-ddump-prep", path)(pwd)
       val dump = read(pwd/'haskellopt/'src/'test/'haskell/s"VectorsBench_$size.dump-prep")
       //val sizes = dump.lines.dropWhile(_ =/= "Result size of CorePrep").drop(1).next
       //println(s"SIZES: $sizes")
@@ -112,19 +138,19 @@ object MeasureSynthBenchSizes extends BenchTests with App {
     }
     val terms0 = termsFor(true)
     val terms1 = termsFor(false)
-    
+
     //???
     //size -> terms
-    
+
     ///Volumes/Macintosh HD/Users/lptk/work/EPFL/DATA/git/alt/Squid-1.0/haskellopt_gen/bench/VectorsBench_1.pass-0000.opt.hs
     ///Volumes/Macintosh HD/Users/lptk/work/EPFL/DATA/git/alt/Squid-1.0/haskellopt_gen/VectorsBench_1.pass-0000.opt.hs
-    
+
     val path2 = pwd/'haskellopt_gen/'bench/s"VectorsBench_$size.pass-0000.opt.hs"
     TestHarness(s"VectorsBench_$size", "0000"::Nil, compileResult = false)
-    %("ghc", "-O2", "-fforce-recomp", "-ddump-to-file", "-ddump-prep", path2)(pwd)
+    %("ghc", opt, "-fforce-recomp", "-ddump-to-file", "-ddump-prep", path2)(pwd)
     val dump2 = read(pwd/'haskellopt_gen/'bench/s"VectorsBench_$size.pass-0000.opt.dump-prep")
     val terms2 = getTerms(dump2.lines)
-    
+
     (size, terms0, terms1, terms2)
   }
   //println(s"Core sizes:${coreSizes.map(is => "\n"+is._1+","+is._2).mkString("")}")
